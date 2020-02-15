0627b2d647797bf30801584d201f71e9764e9163
GMorse19 <malorse@comcast.net>
2020-02-15 13:50:37 -0500

    Adjusted serializer

--- app/serializers/work_space_serializer.rb
+++ app/serializers/work_space_serializer.rb
@@ -1,5 +1,5 @@
 class WorkSpaceSerializer < ActiveModel::Serializer
-  attributes :id, :place_id, :lat, :lng, :user
+  attributes :id, :place_id, :lat, :lng, :reviews, :user
   has_one :user
   has_many :reviews
 end
