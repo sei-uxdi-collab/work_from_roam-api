0b1247958691a274f5220e7a1a0ef78d5882f2e2
GMorse19 <malorse@comcast.net>
2020-04-12 11:09:13 -0400

    Updated user login serializer

--- app/serializers/user_login_serializer.rb
+++ app/serializers/user_login_serializer.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class UserLoginSerializer < ActiveModel::Serializer
-  attributes :id, :email, :token
+  attributes :id, :email, :username, :avatar, :token
 
   def token
     Rails.application.message_verifier(:signed_token).generate(object.token)
