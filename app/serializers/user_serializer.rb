049bb8dcbbb763891654d2bea1a24eadb2d175a9
GMorse19 <malorse@comcast.net>
2020-06-09 22:20:49 -0400

    Added :username and :avatar to reviews serializer

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -2,11 +2,8 @@
 
 class UserSerializer < ActiveModel::Serializer
   attributes :id,
-             :email,
              :avatar,
-             :username,
-             :votes,
-             :reviews
+             :username
   has_many :reviews
   has_many :votes
   has_many :work_spaces
